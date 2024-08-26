21e08161d2973fdd71dcd5d5d76406fa6b2273da
VaN313 <nosov.tor2014@yandex.ru>
2024-08-26 19:41:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+print((9**0.5)*5)  # 1st program
+print(9.99 > 9.98 and 1000 != 100.1)  # 2st program
+print(2*2+2), print(2*(2+2)), print(2*2+2 == 2*(2+2))  # 3st program
+print(int(123.456*10 % 1230.56))  # 4st program
+
